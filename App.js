e753aa8404e1023008fa7fec85f1e68e2f4c85c7
annavasylashko <anna.vasylashko123@gmail.com>
2021-02-09 17:48:40 +0200

    Fix

--- App.js
+++ App.js
@@ -38,7 +38,6 @@ export default function App() {
               iconName = "ios-file-tray-outline";
             }
 
-            // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={color} />;
           },
         })}
